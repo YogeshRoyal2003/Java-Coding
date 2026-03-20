d23b552bc66c6a83721dba19d6af6f55fab10930
Royal <thupakula-yogesh.royal@capgemini.com>
2026-03-20 14:04:00 +0530

    Add EmployeeDetails.java

---
+++ OopsEncapsulation/EmployeeDetails.java
@@ -0,0 +1,28 @@
+package OopsEncapsulation;
+
+public class EmployeeDetails {
+    private String Ename;
+    private String Gmail;
+
+    public EmployeeDetails(String name, String mail){
+        System.err.println("Inside Parameterised constructor");
+        Ename = name;
+        Gmail = mail;
+    }
+
+    public String getEname(){
+        return Ename;
+    }
+
+    public void setEname(String name){
+        Ename = name;
+    }
+
+    public String getGmail() {
+        return Gmail;
+    }
+
+    public void setGmail(String gmail) {
+        Gmail = gmail;
+    }
+}
