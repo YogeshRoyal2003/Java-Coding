14bf6987f236d3b803ce41f83e824a46f0f34328
Royal <thupakula-yogesh.royal@capgemini.com>
2026-03-28 14:33:26 +0530

    Added JDBC folder

---
+++ JDBCConnection/src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+// package org.example;
+package JDBCConnection.src.main.java.org.example;
+
+import java.sql.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        String url = "jdbc:postgresql://localhost:8080/Student";
+        String uname = "postgres";
+        String password = "YogeshRoyal@03";
+        Connection con = DriverManager.getConnection(url, uname, password);
+        System.out.println("Connection Established");
+
+        Statement st = con.createStatement();
+
+        String sql = "SELECT sname, smarks FROM student WHERE sid=1";
+
+        ResultSet rs = st.executeQuery(sql);
+        rs.next();
+
+        String name = rs.getString("sname");
+        int m = rs.getInt("smarks");
+        System.out.println("Name of the Student: " + name + " ,Marks: " + m);
+
+        con.close();
+        System.out.println("Connection Closed!!");
+
+
+
+    }
+}
